374d4220a861e8dd08c9169cbc3a64a0b0ba793e
Carlos Barrios <carlosbarrios148@gmail.com>
2024-10-08 15:36:06 -0500

    Backend initial project

---
+++ gastromanager-backend/src/main/java/com/kaiho/gastromanager/GastroManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kaiho.gastromanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroManagerApplication.class, args);
+	}
+
+}
---
+++ gastromanager-backend/src/test/java/com/kaiho/gastromanager/GastroManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiho.gastromanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
