d90dea6b6a20de2772a0710a1d019ccc5ff7d7ce
Carlos Barrios <carlosbarrios148@gmail.com>
2025-09-22 00:33:07 -0500

    Change to Neon database url

--- gastromanager-backend/src/main/java/com/kaiho/gastromanager/GastroManagerApplication.java
+++ gastromanager-backend/src/main/java/com/kaiho/gastromanager/GastroManagerApplication.java
@@ -2,21 +2,12 @@ package com.kaiho.gastromanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class GastroManagerApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(GastroManagerApplication.class, args);
-        // Print del profile activo
-        System.out.println("Active profiles: " + Arrays.toString(run.getEnvironment().getActiveProfiles()));
-
-        // Print de la propiedad de datasource username
-        String dbUser = run.getEnvironment().getProperty("spring.datasource.username");
-        System.out.println("Datasource username: " + dbUser);
+        SpringApplication.run(GastroManagerApplication.class, args);
     }
 
 }
